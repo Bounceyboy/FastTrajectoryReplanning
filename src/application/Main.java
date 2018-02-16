f278b16c5db669c1de5f6996d54636a13db62fce
Jason Holley <jason.holley@rutgers.edu>
2018-02-16 15:23:31 -0500

    FXML stuff started

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,10 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/Main.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root,900,600);
+			
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("AI Project 1");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
